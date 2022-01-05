9074c49040017e64695195c3f2c9b8681349d703
Kakeru <stand.by.we@gmail.com>
2022-01-05 20:32:30 +0900

    ガワ

---
+++ SwiftUIToolBox/App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIToolBoxApp.swift
+//  SwiftUIToolBox
+//
+//  Created by Kakeru Fukuda on 2022/01/05.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIToolBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUIToolBox/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIToolBox
+//
+//  Created by Kakeru Fukuda on 2022/01/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
